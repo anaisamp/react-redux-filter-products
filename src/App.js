99aede1bbdadad6e315d122447843ecfd8ce4c09
Ana Sampaio <ana.sampaio@farfetch.com>
2018-02-24 17:10:13 +0000

    remove uncessary files

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
 import React, { Component } from 'react';
-import { Route, Link } from 'react-router-dom';
 import Home from './Home';
-import About from './About';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div class="app">
-        <header>
-          <Link to="/">Home</Link>
-          <Link to="/about-us">About</Link>
-        </header>
-
-        <main>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/about-us" component={About} />
-        </main>
+          <Home />
       </div>
     );
   }
--- src/Home.js
+++ src/Home.js
@@ -1,15 +1,14 @@
 import React from 'react';
-import { push } from 'react-router-redux';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
+import classnames from 'classnames';
 import { fetchCategoriesAndProducts } from './modules/categories';
 import { fetchProducts, toogleProductDescriptionVisibility, fetchProductsFilter } from './modules/products';
 import './Home.css';
-import classnames from 'classnames';
 
   class Home extends React.Component {
 
-    handleOnClick = (categoryId) => {
+    handleClickSelectCategory = (categoryId) => {
         this.props.fetchProducts(categoryId);
     }
    
@@ -27,52 +26,49 @@ import classnames from 'classnames';
         }
     }
 
-      render() {
-          return (
-              <div>
-                    <div className="categoriesWrapper">
-                    { 
-                        this.props.categories.map(c => (
-                            <div
-                                key={ c.id } 
-                                data-key={ c.id }
-                                className={ classnames({
-                                    'categoryTitle': true,
-                                    'selected': this.props.selectedCategory === c.id,
-                                    }) } 
-                                onClick={ () => this.handleOnClick(c.id) } 
-                                selected={this.props.selectedCategory === c.id}>
-                                { c.title }
-                            </div>
-                        ))
-                    }
-                    </div>
-                    <input type="search" size="45" 
-                        value= { this.props.searchValue }
-                        onInput={ (e) => this.handleSearch(e.target.value) } />
-                    <div className="productList">
-                    {
-                        this.props.visibleProducts.map((p, i) => (  
-                            <div key={p.id}  className="productWrapper">
-                                <p onClick={ () => this.handleClickExpandDescription(i)}
-                                    className={ `productTitle ${this.props.toogleProductDescription[i] ? 'selected' : ''}`}
-                                >{ p.title }</p>
-                                <p className={  `productDescription ${this.props.toogleProductDescription[i] ? 'show' : 'hide'}`}> 
-                                    { p.description } 
-                                </p>
-                            </div>
-                        ))
-                    }
-                    </div>
-                <p><button onClick={() => this.props.changePage()}>Go to about page via redux</button></p>
-            </div>
-          );
-      }
+    render() {
+        return (
+            <div>
+                <div className="categoriesWrapper">
+                { 
+                    this.props.categories.map(c => (
+                        <div
+                            key={ c.id } 
+                            data-key={ c.id }
+                            className={ classnames({
+                                'categoryTitle': true,
+                                'selected': this.props.selectedCategory === c.id,
+                                }) } 
+                            onClick={ () => this.handleClickSelectCategory(c.id) } 
+                            selected={this.props.selectedCategory === c.id}>
+                            { c.title }
+                        </div>
+                    ))
+                }
+                </div>
+                <input type="search" size="45" 
+                    value= { this.props.searchValue }
+                    onInput={ (e) => this.handleSearch(e.target.value) } />
+                <div className="productList">
+                {
+                    this.props.visibleProducts.map((p, i) => (  
+                        <div key={p.id}  className="productWrapper">
+                            <p onClick={ () => this.handleClickExpandDescription(i)}
+                                className={ `productTitle ${this.props.toogleProductDescription[i] ? 'selected' : ''}`}
+                            >{ p.title }</p>
+                            <p className={  `productDescription ${this.props.toogleProductDescription[i] ? 'show' : 'hide'}`}> 
+                                { p.description } 
+                            </p>
+                        </div>
+                    ))
+                }
+                </div>
+        </div>
+        );
+    }
   }
 
   const mapStateToProps = (state, ownProps) => ({
-    //id: ownProps.params.id,
-    //filter: ownProps.location.query.filter,
     categories: state.categories.categories,
     isFetchingCategories: state.categories.isFetchingCategories,
     errorFetchingCategories: state.categories.errorFetchingCategories,
@@ -90,7 +86,6 @@ import classnames from 'classnames';
     fetchProductsFilter,
     fetchCategoriesAndProducts,
     toogleProductDescriptionVisibility,
-    changePage: () => push('/about-us'),
   }, dispatch)
   
   export default connect(
